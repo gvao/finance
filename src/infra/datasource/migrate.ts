5fc028b7a640441cc1592db3bb1423254b01d371
Yuri Galvão <yuri.gvao@gmail.com>
2026-05-23 13:58:56 -0300

    created migrations helpers

---
+++ src/infra/datasource/migrate.ts
@@ -0,0 +1,35 @@
+import { readdir, readFile } from "node:fs/promises"
+import path from "path"
+import type { Datasource } from "./types.js"
+
+export async function getMigrations(db: Datasource) {
+    try {
+        const migrations = await db.query(`SELECT * FROM migrations`)
+        return migrations
+    } catch (error) {
+        return []
+    }
+}
+
+export async function migrate(dirPath: string, db: Datasource) {
+    const fileNames = await readdir(dirPath)
+
+    const result = []
+
+    for (const fileName of fileNames) {
+        const migrations = await getMigrations(db)
+        const date = new Date()
+        const filePath = path.join(dirPath, fileName)
+        const [order, name] = fileName.split('-') as [string, string]
+        const statement = await readFile(filePath, "utf-8")
+
+        if (migrations.length === 0) {
+            if (order !== "000") continue
+            await db.run(statement)
+        }
+
+        await db.run('INSERT INTO migrations (id, name, create_at) VALUES (?, ?, ?)', [order, name.replace('.sql', ''), date.toISOString()])
+        result.push({ order, name, statement })
+    }
+    return result
+}
