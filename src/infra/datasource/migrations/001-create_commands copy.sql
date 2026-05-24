9ef33451f651b6a8b96fd7c27db312ffe67abd46
Yuri Galvão <yuri.gvao@gmail.com>
2026-05-24 00:59:56 -0300

    updated sql file

--- src/infra/datasource/migrations/001-create_commands copy.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS events (
-    id UUID PRIMARY KEY NOT NULL,
-    command_id UUID
-    type TEXT,
-    data BLOB
-)
--- src/infra/datasource/migrations/002-create_commands.sql
+++ src/infra/datasource/migrations/002-create_commands.sql
@@ -1,5 +1,9 @@
 CREATE TABLE IF NOT EXISTS commands (
     id UUID PRIMARY KEY,
+    aggregate_id UUID,
     type TEXT,
+    status TEXT,
+    updated_at TEXT,
+    created_at TEXT,
     data BLOB
-)
+);
