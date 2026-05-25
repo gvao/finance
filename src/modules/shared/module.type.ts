3819f109e424630f63e04410a04fb0a6e24ed506
Yuri Galvão <yuri.gvao@gmail.com>
2026-05-25 01:10:00 -0300

    feat(shared): define base Module and Dependencies types

---
+++ src/modules/shared/module.type.ts
@@ -0,0 +1,12 @@
+import type { CommandHandler, CommandBus } from "./command/types.js";
+import type { EventPublisher } from "./event/types.js";
+
+export interface Module {
+    name: string;
+    handlers: Record<string, CommandHandler>
+}
+
+export type Dependencies = {
+    commandBus: CommandBus,
+    eventBus: EventPublisher,
+}
