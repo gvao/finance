66e9adf3559b4aed29e842f0b17808f635d2a464
Yuri Galvão <yuri.gvao@gmail.com>
2026-05-27 14:56:00 -0300

    refactor(event): update types to use EventObserver and variadic publish

--- src/modules/shared/event/types.ts
+++ src/modules/shared/event/types.ts
@@ -19,17 +19,14 @@ export type EventRow = {
 }
 
 export interface EventStore {
-    getEvents: ({ endDate, startDate }?: {
-        startDate?: Date;
-        endDate?: Date;
-    }) => Promise<EventRecord[]>;
+    getEvents: (option?: { startDate?: Date; endDate?: Date; }) => Promise<EventRecord[]>;
     appendEvents: (...newEvents: EventRecord[]) => Promise<void>;
     getEventsByAggregateId: (aggregateId: string, startVersion: number) => Promise<EventRecord[]>;
 }
 
-export type EventListener = (...events: EventRecord[]) => void;
+export type EventObserver = (...events: EventRecord[]) => void;
 export interface EventPublisher {
-    publish(events: EventRecord[]): Promise<void>;
-    subscribe(listener: EventListener): void;
-    unsubscribe(listener: EventListener): void;
+    publish(...events: EventRecord[]): void;
+    subscribe(listener: EventObserver): void;
+    unsubscribe(listener: EventObserver): void;
 }
