d2e4611a3fe97b1e694ef821a6ef3504371e1c47
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2026-05-27 03:47:46 +0000

    updated events migrations

--- src/infra/datasource/migrations/001-create_events.sql
+++ src/infra/datasource/migrations/001-create_events.sql
@@ -1,7 +1,14 @@
-CREATE TABLE IF NOT EXISTS events (
-    id UUID PRIMARY KEY NOT NULL,
-    command_id UUID,
-    aggregate_id UUID,
-    type TEXT,
-    data BLOB
-)
+CREATE TABLE events (
+  id TEXT PRIMARY KEY,
+
+  aggregate_id TEXT NOT NULL,
+  version INTEGER NOT NULL,
+
+  command_id TEXT NOT NULL,
+
+  type TEXT NOT NULL,
+
+  payload BLOB NOT NULL,
+
+  created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
