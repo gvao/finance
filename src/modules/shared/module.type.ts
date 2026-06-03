7c6d68910faf811723c2a6094f194d41cacf2409
Yuri Galvão <yuri.gvao@gmail.com>
2026-06-02 23:58:23 -0300

    refactor(shared): update Module interface and Dependencies types

--- src/modules/shared/module.type.ts
+++ src/modules/shared/module.type.ts
@@ -1,12 +1,14 @@
+import type { Datasource } from "../../infra/datasource/types.js";
 import type { CommandHandler, CommandBus } from "./command/types.js";
 import type { EventPublisher } from "./event/types.js";
 
 export interface Module {
     name: string;
-    handlers: Record<string, CommandHandler>
+    commands: Record<string, CommandHandler>
 }
 
 export type Dependencies = {
     commandBus: CommandBus,
     eventPublisher: EventPublisher,
+    datasource: Datasource,
 }
