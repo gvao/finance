5fb79d2a51f0944fbb873b6f3aa7ed459499bc69
Yuri Galvão <yuri.gvao@gmail.com>
2026-06-02 23:58:50 -0300

    feat(finance): update finance module commands and test suite

--- src/modules/finance/index.module.test.ts
+++ src/modules/finance/index.module.test.ts
@@ -1,22 +1,50 @@
-import { beforeAll, describe, it } from "vitest"
+import { beforeAll, beforeEach, describe, expect, it } from "vitest"
+
 import { makeFinanceModule } from "./index.module.js"
-import { composeCommandBus, makeCommandStore } from "../shared/command/command-store.js"
+import { buildCommand, composeCommandBus, makeCommandStore } from "../shared/command/command-store.js"
 import { makeSqliteDatasource } from "../../infra/datasource/sqlite.datasource.js"
 import { composeEventPublisher, makeEventStore } from "../shared/event/event-store.js"
 import type { Module } from "../shared/module.type.js"
 
 describe("finance", () => {
-    let financeModule: Module
+    const aggregate = { id: crypto.randomUUID() }
 
-    beforeAll(() =>  {
-        const datasource = makeSqliteDatasource()
-        const commandBus = composeCommandBus(makeCommandStore(datasource), datasource)
-        const eventPublisher = composeEventPublisher(makeEventStore(datasource))
-        financeModule = makeFinanceModule({commandBus, eventPublisher})
+    let financeModule: Module
+    let commands: string[] = []
+
+    const datasource = makeSqliteDatasource()
+    const eventPublisher = composeEventPublisher(makeEventStore(datasource))
+    const commandBus = composeCommandBus(makeCommandStore(datasource), datasource)
+
+    beforeAll(() => {
+        financeModule = makeFinanceModule({ datasource })
+        commands = Object.keys(financeModule.commands)
     })
 
     it("create recurrence", async () => {
+        const type: keyof typeof commands = "create-recurrence"
+        const eventExpected = { type: "finance:recurrence-created" }
 
+        const command = buildCommand({
+            aggregateId: aggregate.id,
+            type: "create-recurrence",
+            data: {
+                amount: 1000.02
+            }
+        })
+
+        const { commands } = financeModule
+        const commandHandler = commands[type]
+        if (!commandHandler) throw new Error("Handler not found")
+        commandBus.register(type, commandHandler)
+        eventPublisher.subscribe(event => {
+            expect(event).toEqual(eventExpected)
+        })
+
+        const [event] = await commandBus.handle("create-recurrence", command)
+        if (!event) throw new Error("Event not found")
+        eventPublisher.publish(event)
+        expect(event).toEqual(eventExpected)
     })
 
 
--- src/modules/finance/index.module.ts
+++ src/modules/finance/index.module.ts
@@ -1,22 +1,32 @@
-import type { Command } from "../shared/command/types.js";
-import { buildEvent } from "../shared/event/event-store.js";
+import type { UUID } from "node:crypto";
+import type { Command, CommandHandler } from "../shared/command/types.js";
+import { buildEvent, makeEventStore } from "../shared/event/event-store.js";
 import type { Dependencies, Module } from "../shared/module.type.js";
+import type { Aggregate } from "../shared/aggregate/type.js";
 
-export function makeFinanceModule(dependencies: Pick<Dependencies, "eventPublisher">): Module {
+export type Recurrence = Aggregate & {
+    weekDays: number[],
+}
 
-    const commandsExpected = {
-        "create-recurrence": 
+export function makeFinanceModule({ datasource }: Pick<Dependencies, "datasource">): Module {
+    const { getEventsByAggregateId } = makeEventStore(datasource)
+
+    const name = "finance"
+
+    const commands = {
+        "create-recurrence": async (type: string, command: Command) => {
+            const events = await getEventsByAggregateId(command.aggregateId)
+
+            const event = buildEvent({ type: "finance:recurrence-created", aggregateId: command.aggregateId, commandId: command.id, payload: {}, version: events.length + 1 })
+            return [event]
+        },
+        "update-recurrence": async (type: string, command: Command) => {
+            return []
+        }
     }
 
     return {
-        name: "finance",
-        handlers: {
-            "create-recurrence": async (command: Command) => {
-                dependencies
-                const event = buildEvent({ aggregateId: command.aggregateId, type: "recurrence-created", payload: {}, commandId: command.id, version: 1  })
-                
-                return [event]
-            }
-        },
+        name,
+        commands,
     }
 }
